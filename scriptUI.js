e66ad46127c3f74ddb9379ecc027f4a50e77be15
LoptrSir <91218015+LoptrSir@users.noreply.github.com>
2022-07-06 09:10:21 -0700

    creating branch ui1

--- scriptUI.js
+++ scriptUI.js
@@ -1,78 +1,74 @@
 let roundCount = 0;
 let humanWins = 0;
-let compWins = 0;
+let computerWins = 0;
 let person;
 let computer;
+// branch ui1
+//const btn = document.querySelector("#btn");
+const brock = document.createElement("button");
+brock.classList.add("brock");
+brock.innerText = "Rock";
+document.body.appendChild(brock);
+const bpaper = document.createElement("button");
+bpaper.classList.add('bpaper');
+bpaper.innerText = 'Paper';
+document.body.appendChild(bpaper);
+const bscissor = document.createElement("button");
+bscissor.classList.add('bscissor');
+bscissor.innerText = 'Scissors';
+document.body.appendChild(bscissor);
 
 function playGame(person, computer) {
-    roundCount++;
-	person = prompt(
-		'What will it be? Rock, Paper, or Scissors.'
-	);
-	person = person.toUpperCase();
-	computer = [
-		Math.floor(Math.random() * 3) + 1
-	];
-	//console.log(computer)
-	if (computer == 1) {
-		computer = 'ROCK';
-	} else if (computer == 2) {
-		computer = 'PAPER';
-	} else if (computer == 3) {
-		computer = 'SCISSORS';
-	} else {
-		computer = 'No true statements';
-	}
-	console.log(person, computer); // show selections
-	
-	if (person === computer) {
-		console.log(
-			'Tie',
-			roundCount,
-			humanWins,
-			compWins
-		);
-		return 'Tie';
-	} else if (
-		(person === 'ROCK' &&
-			computer === 'SCISSORS') ||
-		(person === 'PAPER' && computer === 'ROCK') ||
-		(person === 'SCISSORS' && computer === 'PAPER')
-	) {
-		humanWins++;
-		console.log(
-			'Human wins this round',
-			roundCount,
-			humanWins,
-			compWins
-		);
-		return 'Human wins this round';
-	} else {
-		compWins++;
-		console.log(
-			'Skynet wins this round',
-			+roundCount,
-			+humanWins,
-			+compWins
-		);
-		return 'Skynet wins this round';
-	}
+  roundCount++;
+  person = prompt("What will it be? Rock, Paper, or Scissors.");
+  person = person.toUpperCase();
+  computer = [Math.floor(Math.random() * 3) + 1];
+  //console.log(computer)
+  if (computer == 1) {
+    computer = "ROCK";
+  } else if (computer == 2) {
+    computer = "PAPER";
+  } else if (computer == 3) {
+    computer = "SCISSORS";
+  } else {
+    computer = "No true statements";
+  }
+  console.log(person, computer); // show selections
+
+  if (person === computer) {
+    console.log("Tie", roundCount, humanWins, computerWins);
+    return "Tie";
+  } else if (
+    (person === "ROCK" && computer === "SCISSORS") ||
+    (person === "PAPER" && computer === "ROCK") ||
+    (person === "SCISSORS" && computer === "PAPER")
+  ) {
+    humanWins++;
+    console.log("Human wins this round", roundCount, humanWins, computerWins);
+    return "Human wins this round";
+  } else {
+    computerWins++;
+    console.log(
+      "Skynet wins this round",
+      +roundCount,
+      +humanWins,
+      +computerWins
+    );
+    return "Skynet wins this round";
+  }
 }
 
-function game() {
-	for (let i = 1; i <= 20; i++)
-		if (humanWins === 5) {
-		console.log('game over. Human wins!');
-			return 'game over';
-		} 
-		else if (compWins === 5) {
-			console.log('game over. Skynet wins!')
-		}
-		else {
-			playGame(person, computer);
-		}
-	//console.log (humanWins, compWins)
-	return humanWins, compWins;
-}
+//function game() {
+//	for (let i = 1; i <= 5; i++)
+//		if (humanWins === 5 || computerWins === 5) {
+//			console.log('game over');
+//			return 'game over';
+//		} else {
+//			playGame(person, computer);
+//		}
+//console.log (humanWins, computerWins)
+//	return humanWins, computerWins;
+//}
 
-game();
+//game();
+playGame();
